b6eb33552d68dbadbf1a317bc76cc1cccd430ce8
natazuchowska <zuchowskanata@gmail.com>
2023-10-20 11:11:35 +0200

    terrain and player controls

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,118 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-    [Header("Movement Refernces")]
-    public float movespeed;
+    public float speed;
+    private Vector2 move;
 
-    public float groundedDrag;
-
-    public float jumpForce;
-    public float jumpCooldown;
-    public float airMultiplier;
-    bool readyToJump;
-
-    [Header("KeyBinds")]
-    public KeyCode jumpKey = KeyCode.Space; //spacebar for jump input
-    
-
-    [Header("Ground Check")]
-    public float playerHeight;
-    public LayerMask whatIsGround;
-    public bool grounded;
-
-    public Transform orientation; // current player orientation
-
-    float horizontalInput;
-    float verticalInput;
-
-    Vector3 moveDir;
-
-    Rigidbody rB;
-
-
-    // Start is called before the first frame update
-    void Start()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        rB = GetComponent<Rigidbody>();
-        rB.freezeRotation = true;
-
-        resetJump();
+        move = context.ReadValue<Vector2>();
     }
 
-    private void FixedUpdate()
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
     {
         MovePlayer();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MovePlayer()
     {
-        //checking if user in touchning the ground
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        Vector3 movement = new Vector3(move.x, 0f, move.y);
 
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
 
-        MyInput();
-        SpeedControl();
-
-        if (grounded)
-            rB.drag = groundedDrag;
-        else
-            rB.drag = 0;
-    }
-
-    public void MyInput()
-    {
-        horizontalInput = Input.GetAxisRaw("Horizontal"); //W and S
-        verticalInput = Input.GetAxisRaw("Vertical"); // A and D
-
-        if(Input.GetKey(jumpKey) && readyToJump && grounded) // if ready to jump is true and the player is grounded
-        {
-            readyToJump = false;
-
-            Jump();
-
-            Invoke(nameof(resetJump), jumpCooldown);
-
-        }
-    }
-
-    private void MovePlayer()
-    {
-        //move Direction
-        moveDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
-
-        //adding force in the direction the user is looking in
-        if(grounded)
-            rB.AddForce(moveDir.normalized * movespeed * 10f, ForceMode.Force);
-        
-        else if(!grounded)
-            rB.AddForce(moveDir.normalized * movespeed * 10f * airMultiplier, ForceMode.Force);
-        
-    }
-
-    private void SpeedControl()
-    {
-        Vector3 flatVel = new Vector3(rB.velocity.x, 0f, rB.velocity.z);
-
-        if(flatVel.magnitude > movespeed) //limiting your velocity
-        {
-            Vector3 limitedVel = flatVel.normalized * movespeed;
-            rB.velocity = new Vector3(limitedVel.x, rB.velocity.y, limitedVel.z);
-        }
-    }
-
-    private void Jump()
-    {
-        rB.velocity = new Vector3(rB.velocity.x, 0f, rB.velocity.z);
-        rB.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-    }
-
-    private void resetJump()
-    {
-        readyToJump = true;
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
     }
 }
